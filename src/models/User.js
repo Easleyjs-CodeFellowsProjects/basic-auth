2d2dc8f54a0ce80e2b9ed7ac1ea859ed22fae75e
Josh Easley <easleyjs@gmail.com>
2023-09-26 23:24:30 -0700

    Refactors User model for basic auth

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,34 @@
 'use strict';
-//const { Sequelize, DataTypes } = require('sequelize');
-const UserModel = (sequelize, DataTypes) => {
-    // create the user Model / Table
-    sequelize.define('Users', {
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
+
+const bcrypt = require('bcrypt');
+
+const userModel = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('Users', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
+
+  // hook: something that occurs automagically -> when an event.
+  userTable.beforeCreate(async (user) => {
+    // encrypt the password
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+  
+  userTable.authenticateBasic = async function (username, password) {
+    let userRecord = await this.findOne({ where: { username }}); // valuse are pulled from DB
+    let valid = await bcrypt.compare(password, userRecord.password);
+    if (valid) {
+      return userRecord;
+    } else {
+      throw new Error('Invalid credentials');
+    }
+  }
 }
 
-module.exports = UserModel;
+module.exports = userModel;
